e432f0782aa14516af26f4410abc4b64748e6be2
seungho.lee729 <seungho.lee729@nts-corp.com>
2022-05-31 02:33:44 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var jwt = localStorage.getItem("jwt");
+if (jwt == null) {
+    window.location.href = './login.html'
+}
+
+function loadUser() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://www.mecallapi.com/api/auth/user");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4) {
+            const objects = JSON.parse(this.responseText);
+            if(objects["status"] == "ok") {
+                const user = objects["user"]
+                document.getElementById("fname").innerHTML = user["fname"];
+            }
+        }
+    }
+}
+
+loadUser();
+
+function logout() {
+    localStorage.removeItem("jwt");
+    window.location.href = './login.html'
+}
